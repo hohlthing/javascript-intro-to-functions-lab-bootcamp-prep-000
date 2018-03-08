e4c40dd35f826495135dc86a9e92a8e3b3078479
michael hohl <mhohl@rocketmail.com>
2018-03-08 18:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function logWhisper(string)  {
 sayHiToGrandma(string)  {
   if string == "I love you, Grandma."
     return "I love you, too."
-  else if string.toLowerCase()
+  else if string == string.toLowerCase()
     return "I can't hear you!"
-  else if 
+  else if string == string.toUpperCase()
+    return "YES INDEED!"
 }
